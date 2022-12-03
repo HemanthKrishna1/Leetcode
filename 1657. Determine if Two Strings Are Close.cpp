d9fdf4d05e0fb15d78ca4854e2f7995d6c02cb11
Hemanth Krishna <42912017+HemanthKrishna1@users.noreply.github.com>
2022-12-03 13:30:37 +0530

    Create 1657. Determine if Two Strings Are Close.cpp

---
+++ 1657. Determine if Two Strings Are Close.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    bool closeStrings(string word1, string word2) {
+        if(word1.length() != word2.length()) return false;
+        vector<int> v1(26,0),v2(26,0),v3(26,0),v4(26,0);
+        for(char c: word1){
+            v1[c - 'a']++;
+            v3[c - 'a'] = 1;
+        }
+        for(char c: word2){
+            v2[c - 'a']++;
+            v4[c - 'a'] = 1;
+        }
+        sort(begin(v1),end(v1));
+        sort(begin(v2),end(v2));
+        return v1==v2 && v3==v4;
+    }
+};
