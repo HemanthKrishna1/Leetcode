6b8f48077e92f172d67b1b588621fc3c781ea49f
Hemanth Krishna <42912017+HemanthKrishna1@users.noreply.github.com>
2022-12-03 14:13:16 +0530

    Update 1704. Determine if String Halves Are Alike.cpp

--- 1704. Determine if String Halves Are Alike.cpp
+++ 1704. Determine if String Halves Are Alike.cpp
@@ -11,45 +11,18 @@ public:
             s2+=s[j];
             j--;
         }    
-        cout << "s1 :" << s1 << endl;
-        cout << "s2 :" << s2 << endl;
-        vector<int> arr1(26*2,0);
-        vector<int> arr2(26*2,0);
-        for(char c: s1){
-            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
-                arr1[c - 'a']++;
-            }
-            if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
-                arr1[c - 'A' + 26]++;
+        int n1 = 0;
+        int n2 = 0;
+        for(char c :s1){
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c =='I' ||c=='O' ||c=='U'){
+                n1++;
             }
         }
-        for(auto it:arr1){
-            cout << it << " ";
-        }
-        cout << endl;
-        
-
-        cout << endl;
-        for(char c : s2){
-            cout << "c :" << c << "-";
-            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u'){
-                arr1[c - 'a']--;
-            }
-            if(c=='A' || c=='E' || c=='I' || c=='O' || c=='U'){
-                arr1[c - 'A' + 26]--;
+        for(char c :s2){
+            if(c=='a' || c=='e' || c=='i' || c=='o' || c=='u' || c=='A' || c=='E' || c =='I' ||c=='O' ||c=='U'){
+                n2++;
             }
         }
-        cout << endl;
-        for(auto it:arr2){
-            cout << it << " ";
-        }
-        int k =0;
-        while(k<26*2){
-            if(arr1[k]!=0){
-                return false;
-            }
-            k++;
-        }
-        return true;
+        return n1==n2 ? true: false;
     }
 };
